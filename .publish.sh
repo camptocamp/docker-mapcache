ea5b6b56f025a3447ade4dbc58d1204f6c6a9b9c
Yves Jacolin <yves.jacolin@camptocamp.com>
2017-10-02 09:31:26 +0200

    Use camptocamp docker repository

--- .publish.sh
+++ .publish.sh
@@ -4,13 +4,13 @@ mkdir ${HOME}\/.docker
 openssl aes-256-cbc -K $encrypted_b6863b582c7b_key -iv $encrypted_b6863b582c7b_iv -in config.json.enc -out ${HOME}\/.docker/config.json -d
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   echo "Deploying image to docker hub for master (latest)"
-  docker push "yjacolin/mapcache:latest"
+  docker push "camptocamp/mapcache:latest"
 elif [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "Deploying image to docker hub for tag ${TRAVIS_TAG}"
-  docker push "yjacolin/mapcache:${TRAVIS_TAG}"
+  docker push "camptocamp/mapcache:${TRAVIS_TAG}"
 elif [ ! -z "$TRAVIS_BRANCH" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "Deploying image to docker hub for branch ${TRAVIS_BRANCH}"
-  docker push "yjacolin/mapcache:${TRAVIS_BRANCH}"
+  docker push "camptocamp/mapcache:${TRAVIS_BRANCH}"
 else
   echo "Not deploying image"
 fi
