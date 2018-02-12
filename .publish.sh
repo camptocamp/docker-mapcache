21938482563637e712f43def507bbb5a9d479cdc
Stéphane Brunner <stephane.brunner@camptocamp.com>
2018-02-12 09:53:03 +0100

    Fix, cleanup

--- .publish.sh
+++ .publish.sh
@@ -1,16 +1,24 @@
 #!/bin/bash -e
 # Setup login
-mkdir ${HOME}\/.docker
-openssl aes-256-cbc -K $encrypted_b6863b582c7b_key -iv $encrypted_b6863b582c7b_iv -in config.json.enc -out ${HOME}\/.docker/config.json -d
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-  echo "Deploying image to docker hub for master (latest)"
-  docker push "camptocamp/mapcache:latest"
-elif [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  echo "Deploying image to docker hub for tag ${TRAVIS_TAG}"
-  docker push "camptocamp/mapcache:${TRAVIS_TAG}"
-elif [ ! -z "$TRAVIS_BRANCH" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  echo "Deploying image to docker hub for branch ${TRAVIS_BRANCH}"
-  docker push "camptocamp/mapcache:${TRAVIS_BRANCH}"
-else
-  echo "Not deploying image"
+
+if  [ "$TRAVIS_PULL_REQUEST" == "false" ]
+then
+    mkdir ${HOME}\/.docker
+    openssl aes-256-cbc -K $encrypted_b6863b582c7b_key -iv $encrypted_b6863b582c7b_iv -in config.json.enc -out ${HOME}\/.docker/config.json -d
+
+    if [ "$TRAVIS_BRANCH" == "$MAIN_BRANCH" ]
+    then
+        echo "Deploying image to docker hub for master (latest)"
+        docker push "camptocamp/mapcache:latest"
+    elif [ ! -z "$TRAVIS_TAG" ]
+    then
+        echo "Deploying image to docker hub for tag ${TRAVIS_TAG}"
+        docker push "camptocamp/mapcache:${TRAVIS_TAG}"
+    elif [ ! -z "$TRAVIS_BRANCH" ]
+    then
+        echo "Deploying image to docker hub for branch ${TRAVIS_BRANCH}"
+        docker push "camptocamp/mapcache:${TRAVIS_BRANCH}"
+    else
+        echo "Not deploying image"
+    fi
 fi
